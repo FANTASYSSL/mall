33d8fb08bf9e88be3146f91a40463973125246dd
FANTASY <platofantasy@163.com>
2018-03-07 22:15:54 +0800

    改版

--- e3-manager/e3-manager-pojo/src/main/java/com/wch/e3mal/pojo/TbOrder.java
+++ e3-manager/e3-manager-pojo/src/main/java/com/wch/e3mal/pojo/TbOrder.java
@@ -1,8 +1,9 @@
 package com.wch.e3mal.pojo;
 
+import java.io.Serializable;
 import java.util.Date;
 
-public class TbOrder {
+public class TbOrder implements Serializable {
     private String orderId;
 
     private String payment;
--- e3-manager/e3-manager-pojo/src/main/java/com/wch/e3mal/pojo/TbUser.java
+++ e3-manager/e3-manager-pojo/src/main/java/com/wch/e3mal/pojo/TbUser.java
@@ -1,8 +1,9 @@
 package com.wch.e3mal.pojo;
 
+import java.io.Serializable;
 import java.util.Date;
 
-public class TbUser {
+public class TbUser implements Serializable {
     private Long id;
 
     private String username;
